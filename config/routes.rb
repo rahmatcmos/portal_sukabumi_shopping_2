a0223715f3274c3c1251e3f003d2db25fe6bd051
fathan <kreasi.fathan@gmail.com>
2014-12-11 01:34:10 +0700

    revision

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'publics#home'
-  get 'catalog', to: 'publics#catalog', as: 'catalog'
-  get 'details_ads', to: 'publics#details_ads', as: 'details_ads'
-  get 'login', to: 'publics#login', as: 'login'
-  get 'sign_up', to: 'publics#sign_up', as: 'sign_up'
+  get 'catalog',      to:     'publics#catalog', as: 'catalog'
+  get 'details_ads',  to: 'publics#details_ads', as: 'details_ads'
+  get 'login',        to:       'publics#login', as: 'login'
+  get 'sign_up',      to:     'publics#sign_up', as: 'sign_up'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
